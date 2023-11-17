f9893cae333ed8adc845d0e519c2b2d319d16c16
hayoungnemo <hylee2@sugarhill.co.kr>
2023-11-17 22:37:11 +0900

    feat: scss 메소드들 사용하여 layout 설계

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
-import './App.css';
+import './scss/style.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div id='wrap'>
+			<header>
+				<h1 className='logo'>
+					<img src={logo} alt='logo' />
+				</h1>
+				<ul className='gnb'>
+					{['home', 'about react', 'board'].map((el) =>
+						el === 'about react' ? (
+							<li>
+								<a href='/' target='_blank'>
+									{el} :Learn React
+								</a>
+							</li>
+						) : (
+							<li>
+								<a href='/'>{el}</a>
+							</li>
+						)
+					)}
+				</ul>
+			</header>
+			<main>
+				<h2>메인</h2>
+				<aside>
+					<p>사이드메뉴</p>
+				</aside>
+			</main>
+			<footer>
+				<div className='footer-div'>푸터: @copyright 치맥녀</div>
+				<div className='footer-div'>컬러가 칠해지는 푸터입니다.</div>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
